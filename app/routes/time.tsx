8a7820941bf2baa047ac57e015c5738726a26493
wangwupeng <netfishx@gmail.com>
2024-12-28 20:25:49 +0900

    init

--- app/routes/time.tsx
+++ app/routes/time.tsx
@@ -1,13 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { useServerFn } from "@tanstack/start";
+import { getServerTime } from "~/utils/time";
 
 export const Route = createFileRoute("/time")({
 	component: Time,
 });
 
 function Time() {
+	const getTime = useServerFn(getServerTime);
+	const { data } = useQuery({
+		queryKey: ["time"],
+		queryFn: () => getTime(),
+	});
 	return (
 		<div className="p-2">
 			<h3>Time</h3>
+			<p>{data}</p>
 		</div>
 	);
 }
